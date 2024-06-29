1474a410f65c440576141a4b3f06114d31ab6bc5
teemo <eru7921129@gmail.com>
2024-06-29 14:10:40 +1200

    metadata

--- eleventy/_data/metadata.js
+++ eleventy/_data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "The Alchemist",
+	url: "https://www.the-alchemist.link",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "'Im coming, Fatima,' he said.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Teemo Dai",
+		email: "teeemo.dai@gmail.com",
+		url: "https://www.the-alchemist.link"
 	}
 }
