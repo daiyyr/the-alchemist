38a967707c09f7d4b0678f2007a03226ecc542b0
Teemo <eru7921129@gmail.com>
2024-08-01 09:50:43 +1200

    serve both the-alchemist.link and www.the-alchemist.link

--- lambda/budget_alert_action.py
+++ lambda/budget_alert_action.py
@@ -3,39 +3,43 @@
 import boto3
 
 zone_id = 'Z06958611JDYVCG41K93R'
-record_name = 'the-alchemist.link.'
+record_names = ['the-alchemist.link.', 'www.the-alchemist.link.']
 record_type = 'A'
 aliasTarget = {}
 
 def del_record():
     r53 = boto3.client('route53')
+    responses = []
 
-    # find the record's AliasTarget
-    paginator = r53.get_paginator('list_resource_record_sets')
-    source_zone_records = paginator.paginate(HostedZoneId=zone_id)
-    for record_set in source_zone_records:
-        for record in record_set['ResourceRecordSets']:
-            if record['Type'] == record_type and record['Name'] == record_name:
-                aliasTarget = record['AliasTarget']
-                print(f"find aliasTarget: {aliasTarget}")
-                break
+    for record_name in record_names:
+        # find the record's AliasTarget
+        paginator = r53.get_paginator('list_resource_record_sets')
+        source_zone_records = paginator.paginate(HostedZoneId=zone_id)
+        for record_set in source_zone_records:
+            for record in record_set['ResourceRecordSets']:
+                if record['Type'] == record_type and record['Name'] == record_name:
+                    aliasTarget = record['AliasTarget']
+                    print(f"find aliasTarget: {aliasTarget}")
+                    break
 
-    response = r53.change_resource_record_sets(
-        HostedZoneId=zone_id,
-        ChangeBatch={ 
-            'Changes': [
-                {
-                    'Action': 'DELETE',
-                    'ResourceRecordSet': {
-                        'Name': record_name,
-                        'Type': record_type,
-                        'AliasTarget': aliasTarget
+        response = r53.change_resource_record_sets(
+            HostedZoneId=zone_id,
+            ChangeBatch={ 
+                'Changes': [
+                    {
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': record_name,
+                            'Type': record_type,
+                            'AliasTarget': aliasTarget
+                        }
                     }
-                }
-            ]
-        }
-    )
-    return response
+                ]
+            }
+        )
+        responses.append(response)
+        
+    return responses
 
 def lambda_handler(event, context):
     print(f"budget_alert_action lambda triggered. event: {event}; context: {context}")
